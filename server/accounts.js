c530e24673feeae5b063c561d21ad298e2be6e0d
natewill <nwilliams1496815@gmail.com>
2025-10-28 16:14:50 -0400

    added sign up function in javascript

--- server/accounts.js
+++ server/accounts.js
@@ -1,6 +1,9 @@
 const argon2 = require("argon2");
 const db = require("./connection");
 
+//returns argon2id hashed pw
+//params
+//user_pw - plain text password as a string
 async function hashPassword(user_pw){
     const hashed = await argon2.hash(user_pw, {
         type: argon2.argon2id,
@@ -13,6 +16,10 @@ async function hashPassword(user_pw){
     return hashed
 }
 
+//returns true if plain password matches hash
+//params
+//hash - argon2id produced hash
+//plain - plain text password as a string
 async function verifyPassword(hash, plain){
     try {
     const match = await argon2.verify(hash, plain);
@@ -23,6 +30,15 @@ async function verifyPassword(hash, plain){
   }
 }
 
+//returns dict if email in database of
+//{
+//  user_password: ____
+//  runner_id: ____
+//}
+// otherwise returns null
+//
+//params
+//email - email of user as a string
 function getPwAndIdFromEmail(email) {
   return new Promise((resolve, reject) => {
     db.query(
@@ -37,6 +53,9 @@ function getPwAndIdFromEmail(email) {
 //returns -1 if email not in database
 //returns -2 if email in database but wrong password
 //returns runner_id if email in database and correct password
+//params 
+//email - email of user as a string
+//plain_pw - non hashed password as a string
 async function login(email, plain_pw){ 
   const runnerInDb = await getPwAndIdFromEmail(email);
   if(!runnerInDb){
@@ -50,12 +69,96 @@ async function login(email, plain_pw){
   }
 }
 
+//params
+//- runner_data
+/*
+{
+    first_name: string,
+    last_name: string,
+    middle_initial, string,
+    email, string,
+    user_password, null,
+    is_leader, bool,
+    min_pace, int,
+    max_pace, int,
+    min_dist_pref, int,
+    max_dist_pref, int
+}
+*/
+// - plain_pw - unhashed plain pw as string
+async function signup(runner_data, plain_pw){
+  //check if email in database already
+  //with the getPwandID function before running this function
+  //then run this function
+  //check if email already in database in this function just incase, return -1
+  //else
+  //hash password
+  //insert into database
+  //return runner id if everything works out
+
+  var emailInDb = await getPwAndIdFromEmail(runner_data.email);
+
+  if(emailInDb){
+    return -1
+  } 
+
+  runner_data.user_password = await hashPassword(plain_pw)
+
+  return new Promise((resolve, reject) => {
+    db.query(
+      "INSERT INTO runners SET ?", runner_data, (err, result) => {
+        if (err) return reject(err)
+        resolve(result.insertId);
+      }
+    )
+  })
+}
+
 
 
 
 //test
-
+/*
 async function tests(){
+
+    const runners = [
+    {
+      first_name: "Nate",
+      middle_initial: "D",
+      last_name: "Williams",
+      email: "natewilliams@vt.edu",
+      user_password: null, 
+      is_leader: true,
+      min_pace: 7,
+      max_pace: 8,
+      min_dist_pref: 3,
+      max_dist_pref: 5
+    },
+    {
+      first_name: "Lara",
+      middle_initial: "K",
+      last_name: "Johnson",
+      email: "lara.k.johnson@example.com",
+      user_password: null,
+      is_leader: false,
+      min_pace: 6,
+      max_pace: 7,
+      min_dist_pref: 2,
+      max_dist_pref: 10
+    },
+    {
+      first_name: "Ethan",
+      middle_initial: "M",
+      last_name: "Garcia",
+      email: "ethan.m.garcia@example.org",
+      user_password: null,
+      is_leader: true,
+      min_pace: 8,
+      max_pace: 9,
+      min_dist_pref: 4,
+      max_dist_pref: 12
+    }
+  ];
 
   const pw1 = "walkthedog2nite!"
   const hash1 = "$argon2id$v=19$m=16,t=2,p=1$cnVubmluZzE$8Bndt/gryd5p4eOTO9YC8A"
@@ -64,6 +167,11 @@ async function tests(){
 
   console.log(await login("natewilliams@vt.edu", "trainspotter_06"))
 
+  runner = {
+
+  }
+  console.log(await signup(runners[2], "boodfagdas"))
 }
 
 tests()
+*/
