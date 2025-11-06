08d415ef4534c7d00bd907073ce2f3c0c4819e36
natewill <nwilliams1496815@gmail.com>
2025-11-06 13:13:14 -0500

    added addresses display when clicking map for start and end if address within 30ms

--- client/src/pages/api/coordinatesToAddress.js
+++ client/src/pages/api/coordinatesToAddress.js
@@ -1,22 +1,41 @@
+import { getDistance } from 'geolib';
+
+
 export async function coordinatesToAddress(lat, lng) {
-  // We do API call on the server
-  const response = await fetch(
-    `/api/reverse-geocode?lat=${encodeURIComponent(
-      lat
-    )}&lng=${encodeURIComponent(lng)}`
-  );
+ // We do API call on the server
+ const response = await fetch(
+   `/api/reverse-geocode?lat=${encodeURIComponent(
+     lat
+   )}&lng=${encodeURIComponent(lng)}`
+ );
 
-  // API return json object
-  const data = await response.json();
 
-  if (data.status === "OK" && data.results.length > 0) {
-    const result = data.results[0];
-    return {
-      formattedAddress: result.formatted_address,
-      placeId: result.place_id,
-    };
-  } else {
-    console.error("Reverse geocoding failed:", data.status, data.error_message);
-    return null;
-  }
+ // API return json object
+ const data = await response.json();
+
+
+ if (data.status === "OK" && data.results.length > 0) {
+   const result = data.results[0];
+
+
+   result.latitude = result.geometry.location.lat;
+   result.longitude = result.geometry.location.lng;
+
+
+   const distance = getDistance({ latitude: lat, longitude: lng },
+     { latitude: result.latitude, longitude: result.longitude });
+
+
+   const within30meters = distance <= 30;
+
+
+   return {
+     within30meters,
+     formattedAddress: result.formatted_address,
+     placeId: result.place_id,
+   };
+ } else {
+   console.error("Reverse geocoding failed:", data.status, data.error_message);
+   return null;
+ }
 }
